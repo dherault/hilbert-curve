01468daade192dd16ae7d52b9e2ba38059a4cadd
David Hérault <dherault@gmail.com>
2020-03-07 16:10:56 +0100

    A piece of art is born

---
+++ index.js
@@ -0,0 +1,71 @@
+const halfPi = Math.PI / 2
+const canvas = document.getElementsByTagName('canvas')[0]
+const _ = canvas.getContext('2d')
+
+const width = canvas.width = window.innerWidth
+const height = canvas.height = window.innerHeight
+
+const size = Math.min(width, height)
+const xStart = width / 2
+const yStart = height / 2
+
+_.fillStyle = 'white'
+_.strokeStyle = 'black'
+_.lineWidth = 1
+
+function draw() {
+  _.fillRect(0, 0, width, height)
+
+  createHilbertCurve(4)(xStart, yStart, size)
+
+  _.stroke()
+}
+
+function createHilbertCurve(maxDepth = 1) {
+
+  function createCurveHyperParameters(size, rotation, invert) {
+    const s = size / 2
+    const z = s / 2
+    const rotationXorInvert = rotation && !invert || !rotation && invert
+
+    const hyperParameters = [
+      [-z, z, s, halfPi, rotationXorInvert],
+      [-z, -z, s, 0, rotationXorInvert],
+      [z, -z, s, 0, rotationXorInvert],
+      [z, z, s, -halfPi, rotationXorInvert],
+    ]
+
+    return rotationXorInvert ? hyperParameters.reverse() : hyperParameters
+  }
+
+  function drawCurve(x, y, size, rotation = 0, invert = false) {
+    _.save()
+    _.translate(x, y)
+    _.rotate(rotation)
+
+    createCurveHyperParameters(size, rotation, invert).forEach(hyperParameter => {
+      _.lineTo(...hyperParameter)
+    })
+
+    _.restore()
+  }
+
+  function drawPart(x, y, size, rotation = 0, invert = false, depth = 1) {
+    _.save()
+    _.translate(x, y)
+    _.rotate(rotation)
+
+    const fn = depth === maxDepth ? drawCurve : drawPart
+    const lastArg = depth === maxDepth ? undefined : depth + 1
+
+    createCurveHyperParameters(size, rotation, invert).forEach(hyperParameter => {
+      fn(...hyperParameter, lastArg)
+    })
+
+    _.restore()
+  }
+
+  return drawPart
+}
+
+window.addEventListener('load', draw)
